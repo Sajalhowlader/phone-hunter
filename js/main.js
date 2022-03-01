a661495b4a25bd2502306eee8b99ba01d8164a6f
Sajalhowlader <sajolhowlader08@gmail.com>
2022-03-01 20:58:10 +0600

    add slice

--- js/main.js
+++ js/main.js
@@ -17,16 +17,18 @@ const searchItem = () => {
     } else if (inputValue !== '') {
         noResult.innerText = ``
         inputField.value = ''
+        detailsContainer.textContent = ""
         mainContainer.textContent = ''
         const searchUrl = ` https://openapi.programming-hero.com/api/phones?search=${inputValue}`
         fetch(searchUrl)
             .then(res => res.json())
-            .then(data => showItems(data.data))
+            .then(data => showItems(data.data.slice(0, 20)))
+
     }
 }
 
 const showItems = items => {
-    console.log(items)
+
     if (items.length === 0) {
         noResult.innerText = `No Results Found`
     } else if (items.length > 0) {
@@ -35,27 +37,27 @@ const showItems = items => {
         brandName.classList.add('text-center')
         brandName.innerText = `Brand: ${items[0].brand}`
         mainContainer.appendChild(brandName)
+
         items.forEach(item => {
             const cardContainer = document.createElement('div');
-
-            cardContainer.classList.add('col-md-4');
+            cardContainer.classList.add('col-md-4', 'col-12');
             cardContainer.innerHTML = `
         <div class="card-group">
-                    <div class="card card-item">
-                        <img src="${item.image}" class="card-img-top" alt="...">
-                   <div class="card-body">
-            <h2 class="card-title text-center">${item.phone_name}</h2>
-            <h4 class="card-text text-center">Brand: ${item.brand}</h4></div>
+        <div class="card card-item">
+        <img src="${item.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h2 class="card-title text-center">${item.phone_name}</h2>
+        <h4 class="card-text text-center">Brand: ${item.brand}</h4>
+        </div>
         <button onclick="getDetails('${item.slug}')" class="details-btn">Details</button>
-            </div>
-         </div>`;
+        </div>
+        </div>`;
             mainContainer.appendChild(cardContainer)
 
         })
     }
 }
 const getDetails = itemDetais => {
-    // console.log(itemDetais)
     detailsContainer.textContent = ""
     const getUrl = `https://openapi.programming-hero.com/api/phone/${itemDetais}`
     fetch(getUrl)
@@ -64,22 +66,20 @@ const getDetails = itemDetais => {
 
 }
 const showDetails = details => {
-    console.log(details.others)
     const sensorDetail = details.mainFeatures.sensors;
     const othersDetails = details.others
-    // const detailsContainer = document.getElementById('details-container')
     const mainDetails = document.createElement('div')
     mainDetails.innerHTML = `<div class="card mb-3">
-    <img src="${details.image}" class="card-img-top" alt="...">
-    <div class="card-body ">
+        <img src="${details.image}" class="card-img-top" alt="...">
+        <div class="card-body">
         <h5 class="card-title title-head">${details.name}</h5>
-        <h6 class="card-title title-head">${details.releaseDate}</h6>
-        <h2>Main Feature</h2>
-        <p>storage:${details.mainFeatures.storage}</p>
-        <p>displaySize:${details.mainFeatures.displaySize}</p>
-        <p><h6>chipset:</h6>${details.mainFeatures.chipSet}</p>
-        <p>memory:${details.mainFeatures.memory}</p> 
-        <h2>Sensor Details</h2>
+        <h6>${details.releaseDate}</h6>
+        <h4>Main Feature</h4>
+        <p>storage: ${details.mainFeatures.storage}</p>
+        <p>displaySize: ${details.mainFeatures.displaySize}</p>
+        <p>chipset: ${details.mainFeatures.chipSet}</p>
+        <p>memory: ${details.mainFeatures.memory}</p> 
+        <h4>Sensor Details</h4>
         <p>${sensorDetail[0]}</p>
         <p>${sensorDetail[1]}</p>
         <p>${sensorDetail[2]}</p>
@@ -87,16 +87,14 @@ const showDetails = details => {
         <p>${sensorDetail[4]}</p>
         <p>${sensorDetail[5]}</p>
         <h4>Others Info</h4>
-        <p>${othersDetails.WLAN}</p>
-        <p>${othersDetails.Bluetooth}</p>
-        <p>${othersDetails.GPS}</p>
-        <p>${othersDetails.Radio}</p>
+        <p>Wlan: ${othersDetails?.WLAN}</p>
+        <p>Bluetooth: ${othersDetails?.Bluetooth}</p>
+        <p>GPS: ${othersDetails?.GPS}</p>
+        <p>Radio: ${othersDetails?.Radio}</p>
         </div>
         </div>
 
     `;
-    detailsContainer.appendChild(mainDetails);
 
+    detailsContainer.appendChild(mainDetails);
 }
-// ['Face ID', 'accelerometer', 'gyro', 'proximity', 'compass', 'barometer']
-// {WLAN: 'Wi-Fi 802.11 a/b/g/n/ac/6, dual-band, hotspot', Bluetooth: '5.0, A2DP, LE', GPS: 'Yes, with A-GPS, GLONASS, GALILEO, BDS, QZSS', NFC: 'Yes', Radio: 'No', …}
